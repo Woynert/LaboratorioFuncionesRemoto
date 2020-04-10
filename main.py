ded3902d4d083b46d591436d7c5a9fbc81953dd4
Woynert <whayner.porras.2020@upb.edu.co>
2020-04-10 09:26:09 -0500

    Agregados contadores y soporte para errores de formato

--- main.py
+++ main.py
@@ -14,13 +14,35 @@ def a_power_b(_a, _b):
 
 a = 1
 b = 1
-cont = 1;
+cont = 1
+nCal = 0
+nPar = 0
+nImp = 0
+nErr = 0
+res = 0
 
+#Bucle 
 while (a != 0):
-    print("\nPOTENCIA",cont,"-------------------------")
-    a = int(input("Ingrese el numero a: \n"))
-    if a != 0:
-        b = int(input("Ingrese el numero b: \n"))
-        print("-->", a_power_b(a, b))
-        cont = cont+1
-
+    print("\nPOTENCIA",cont,"___________________________")
+    try:
+        a = int(input("Ingrese el numero a: \n"))
+        if a != 0:
+            b = int(input("Ingrese el numero b: \n"))
+            res = a_power_b(a, b)
+            
+            #Comprobar paridad 
+            if (res % 2 == 0):
+                nPar = nPar +1
+            else:
+                nImp = nImp +1
+                
+            print("-->", res)
+            cont = cont +1
+            nCal = nCal +1
+    except:
+        print("ERROR: Por Favor Vuelva A Intentar")
+        a = 1
+        nErr = nErr +1
+print("\n___________________________________")
+print("Resumen\nPotencias calculadas:",nCal,"\nPotencias pares:",
+      nPar,"\nPotencias impares:",nImp,"\nErrores:",nErr)
