7337ddab531f26200d74e5ff5f11132298d9e566
Woynert <whayner.porras.2020@upb.edu.co>
2020-04-10 08:57:51 -0500

    Agregado bucle y cierre al digitar cero

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-#Funcion de potencia de numeros enteros
+#Bucle de funcion de potencia de numeros enteros
 
-
-def a_powe_b(_a, _b):
+def a_power_b(_a, _b):
     acu = 1
     
     if (_b != 0):
@@ -13,8 +12,15 @@ def a_powe_b(_a, _b):
             
     return(acu)
 
-a = int(input("Ingrese el numero a: "))
-b = int(input("Ingrese el numero b: "))
+a = 1
+b = 1
+cont = 1;
 
-print(a_powe_b(a, b))
+while (a != 0):
+    print("\nPOTENCIA",cont,"-------------------------")
+    a = int(input("Ingrese el numero a: \n"))
+    if a != 0:
+        b = int(input("Ingrese el numero b: \n"))
+        print("-->", a_power_b(a, b))
+        cont = cont+1
 
